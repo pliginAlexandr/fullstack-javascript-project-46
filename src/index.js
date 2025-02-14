ad72bf7aced88636455fc52483118e449c179e37
Alexandr Pligin <rogchik@gmail.com>
2025-02-14 15:30:22 +0500

    Added flat file comparison

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import parceData from './parcer.js';
 
 export default (filepath1, filepath2) => {
-  const content1 = parceData(filepath1);
-  console.log(content1);
-  const content2 = parceData(filepath2);
-  console.log(content2);
-  return content1 === content2;
+  const data1 = parceData(filepath1);
+  const data2 = parceData(filepath2);
+
+
+  const keys = [...new Set([ ...Object.keys(data1), ...Object.keys(data2)])].sort();
+  const diff = keys.map((key) => {
+    if(!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    };
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    };
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    };
+
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${diff.join('\n')}\n}`;
 };
