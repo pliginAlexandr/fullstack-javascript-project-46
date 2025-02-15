98d0fc8c3abdd5097f2d0afbc1f7af009c04ca2c
Alexandr Pligin <rogchik@gmail.com>
2025-02-15 15:15:17 +0500

    multi import removed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,18 @@
-import gendiff from '../src/index.js'
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const getJsonPath = (filename) => path.join(__dirname, '..', filename)
+const getJsonPath = (filename) => path.join(__dirname, '..', filename);
 const result = fs.readFileSync(getFixturePath('result.txt'), 'utf-8');
 
 test('genddiff', () => {
-    expect (gendiff(
+  expect(gendiff(
     getJsonPath('file1.json'),
-    getJsonPath('file2.json')
-    )).toBe(result);
+    getJsonPath('file2.json'),
+  )).toBe(result);
 });
