a85af15138903ae18a09636f15106ced5d9eb8f5
Alexandr Pligin <rogchik@gmail.com>
2025-02-15 11:41:25 +0500

    Makefile

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ export default (filepath1, filepath2) => {
   const data1 = parceData(filepath1);
   const data2 = parceData(filepath2);
 
-
-  const keys = [...new Set([ ...Object.keys(data1), ...Object.keys(data2)])].sort();
+  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
   const diff = keys.map((key) => {
-    if(!Object.hasOwn(data2, key)) {
+    if (!Object.hasOwn(data2, key)) {
       return `  - ${key}: ${data1[key]}`;
-    };
+    }
     if (!Object.hasOwn(data1, key)) {
       return `  + ${key}: ${data2[key]}`;
-    };
+    }
     if (data1[key] !== data2[key]) {
       return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-    };
+    }
 
     return `    ${key}: ${data1[key]}`;
   });
