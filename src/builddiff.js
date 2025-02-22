b8540da6ddcf6d73103643cedfd8ea204e7d1d50
Alexandr Pligin <rogchik@gmail.com>
2025-02-22 10:52:58 +0500

    stylisher add

---
+++ src/builddiff.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+
+const buildDiff = (obj1, obj2) => {
+  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])].sort();
+
+  return keys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return { key, type: 'added', value: obj2[key] };
+    }
+    if (!_.has(obj2, key)) {
+      return { key, type: 'removed', value: obj1[key] };
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, type: 'nested', children: buildDiff(obj1[key], obj2[key]) };
+    }
+    if (!_.isEqual(obj1[key], obj2[key])) {
+      return {
+        key, type: 'updated', oldValue: obj1[key], newValue: obj2[key],
+      };
+    }
+    return { key, type: 'unchanged', value: obj1[key] };
+  });
+};
+
+export default buildDiff;
---
+++ src/formatters/stylish.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+
+const indentSize = 4;
+
+const getIndent = (depth) => '..'.repeat(depth * indentSize - 2);
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return String(value);
+  }
+  const lines = Object.entries(value)
+    .map(([key, val]) => `${getIndent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
+
+  return `{\n${lines.join('\n')}\n${getIndent(depth)}  }`;
+};
+
+const stylish = (diff, depth = 1) => {
+  const result = diff.map((node) => {
+    const indent = getIndent(depth);
+    switch (node.type) {
+      case 'added':
+        return `${indent}+ ${node.key}: ${stringify(node.value, depth)}`;
+      case 'removed':
+        return `${indent}- ${node.key}: ${stringify(node.value, depth)}`;
+      case 'updated':
+        return `${indent}- ${node.key}: ${stringify(node.oldValue, depth)}\n${indent}+ ${node.key}: ${stringify(node.newValue, depth)}`;
+      case 'nested':
+        return `${indent}  ${node.key}: {\n${stylish(node.children, depth + 1)}\n${indent}  }`;
+      default:
+        return `${indent}  ${node.key}: ${stringify(node.value, depth)}`;
+    }
+  });
+
+  return result.join('\n');
+};
+
+export default stylish;
